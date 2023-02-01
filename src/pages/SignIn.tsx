3c3a506f88c04120fc418f9b562fb06da3daadb8
JJ Seay <greenmajesti@gmail.com>
2023-02-01 13:29:00 -0500

    basic sign-in form
    next goal -> get data from fields when button clicked

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import PageTemplate from '../pages/PageTemplate';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
+
+import { useState } from 'react';
 
 export default function SignIn() {
+	
     return (
         <PageTemplate>
             <h1 style={{textAlign: "center"}}>Sign In</h1>
+			
+			<div className="Auth-form-container">
+			
+			<TextField
+				id="outlined-basic"
+				label="Username"
+				variant="filled"
+			/>
+			
+			<TextField
+				id="filled-password-input"
+				label="Password"
+				type="password"
+				autoComplete="current-password"
+				variant="filled"
+			/>
+			
+			<Button
+				color='secondary'
+				variant="contained"
+				onClick={() => {
+					alert('Trigger login');
+				}}
+			>
+				Sign In
+			</Button>
+			
+			</div>
+			
         </PageTemplate>
     );
 }
