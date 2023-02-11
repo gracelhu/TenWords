421e85a14dfcf72c071bbaceace21ddce927eea1
Grace Hu <gracehu2003@gmail.com>
2023-02-11 11:38:05 -0500

    delete 2 fields from Word struct

--- restapi.go
+++ restapi.go
@@ -23,8 +23,6 @@ type Word struct {
 	ID              string `json:"id"`
 	Word            string `json:"word"`
 	Spanish         string `json:"spanish"`
-	Definition      string `json:"definition"`
-	Partofspeech    string `json:"partofspeech"`
 	Examplesentence string `json:"examplesentence"`
 }
 
