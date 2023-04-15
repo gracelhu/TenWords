ea2b7049ad271c6a3ca24e9dd6477e1a76df8c45
coderliv <oliviabronstein@ufl.edu>
2023-04-15 12:36:09 -0400

    typo

--- src/pages/Words.tsx
+++ src/pages/Words.tsx
@@ -146,7 +146,7 @@ function Words() {
     }
 
     const changeDate = (e: any) => {
-        const original_date = localStorage.get("date");
+        const original_date = localStorage.getItem("date");
         const date = (e.$M + 1).toString().padStart(2,'0') + "-" + e.$D.toString().padStart(2,'0') + "-" + e.$y;
         const daysBetween = new Date(date).getDate() - new Date(original_date).getDate() + 1;
         setPackageNumber(daysBetween);
