09af9f9d37487484fbf6e4259fbabbf2845ff246
Grace Hu <gracehu2003@gmail.com>
2023-04-14 23:48:43 -0400

    update welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,6 +6,7 @@ export default function Welcome() {
         <PageTemplate>
             <h1 style={{textAlign: "center"}}>TenWords</h1>
             <p style={{textAlign: "center"}}>TenWords is an application that helps you to learn a language with just <b>ten</b> words a day.</p>
+            <p style={{textAlign: "center"}}>Head over to the 'Your Words' tab to start learning!</p>
         </PageTemplate>
     );
 }
