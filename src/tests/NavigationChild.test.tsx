85dc4ede74b4d809db8fb2f0ab0f966323dd8fff
coderliv <oliviabronstein@ufl.edu>
2023-04-19 14:59:19 -0400

    Fix navigation tests

--- src/tests/NavigationChild.test.tsx
+++ src/tests/NavigationChild.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {fireEvent, render, screen} from '@testing-library/react'
-import Navigation from '../components/Navigation';
+import NavigationChild from '../components/NavigationChild';
 import { BrowserRouter } from 'react-router-dom';
 
 
 test('Home tab correctly displays', () => {
-    const navigation = render(<Navigation/>);
+    const navigation = render(<NavigationChild logout={() => {}}/>);
     const tab = navigation.getByTestId('home');
     expect(tab).toHaveTextContent("Home");
 });
 
 test('Navigation tab correctly displays', () => {
-    const navigation = render(<Navigation/>);
+    const navigation = render(<NavigationChild logout={() => {}}/>);
     const tab = navigation.getByTestId('your-words');
     expect(tab).toHaveTextContent("Your Words");
 });
